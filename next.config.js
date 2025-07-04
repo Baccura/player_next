f1fcced5c9fd75aee9beb2cdfe02baf67d8f0c57
baccura <baccura.corp@gmail.com>
2025-07-04 15:44:42 +0200

    correction for build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['image.tmdb.org', 'localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
+  webpack: (config, { dev, isServer }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    }
+
+    return config;
+  },
+  transpilePackages: ['lucide-react'],
 }
 
 module.exports = nextConfig
